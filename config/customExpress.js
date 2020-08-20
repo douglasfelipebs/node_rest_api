78a92a9e5770bf66c935c8f0f2081542c7e4c6ed
Douglas Felipe Bussmann Siedschlag <douglasfelipebs@hotmail.com>
2020-08-19 22:18:52 -0300

    [ADD] #insere - Rota de inserção

--- config/customExpress.js
+++ config/customExpress.js
@@ -4,6 +4,7 @@ const consign = require('consign');
 module.exports = () => {
     const app = express();
 
+    app.use(express.json());
     consign().include('controllers').into(app);
 
     return app;
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,20 @@
+const db = require('monk')('localhost:27017/node_rest_api');
+const atendimentos = db.get('atendimentos');
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => {
-        res.send('Você está na rota de atendimentos!');
-    });
+        res.send({
+            att: atendimentos.find({})
+        });
+    })
+
+    app.post('/atendimentos', (req, res) => {
+        console.log('req\t', req.body, '\n\n');
+        const {
+            atendimento
+        } = req.body;
+        res.send({
+            success: atendimentos.insert(atendimento)
+        });
+    })
 }
